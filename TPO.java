ab6ebbfe7ee35f44bfec3ed1a3ae391606f723bf
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-11-15 18:31:04 -0300

    Main Program

---
+++ TPO.java
@@ -0,0 +1,17 @@
+import abstractos.Grafo;
+
+public class TPO {
+  public static void main(String[] args) {
+
+    Grafo<Integer> grafo = new Grafo<Integer>();
+
+    Datos.cargarVertices(grafo);
+    Datos.cargarRutas(grafo);
+
+    // grafo.print();
+
+    System.out.println(Datos.CLIENTES);
+  }
+
+  
+}
