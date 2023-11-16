7b1b8cfd66a1dbb01ad42af39647ed8c7e789f0d
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-11-16 20:00:20 -0300

    Dijkstra Implementation

---
+++ Dijkstra.java
@@ -0,0 +1,60 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import abstractos.Arista;
+import abstractos.Centro;
+import abstractos.Grafo;
+import abstractos.Vertice;
+
+public class Dijkstra {
+  public Vertice origen;
+  private Map<Vertice, Integer> distancias;
+  private PriorityQueue<Vertice> colaPrioridad;
+
+  Dijkstra(Grafo grafo, Vertice origen) {
+    this.origen = origen;
+    distancias = new HashMap<>();
+    colaPrioridad = new PriorityQueue<>(Comparator.comparingInt(distancias::get));
+
+    // Inicialización de distancias
+    for (Vertice v : grafo.getVertices()) {
+      distancias.put(v, Integer.MAX_VALUE);
+    }
+
+    distancias.put(origen, 0);
+
+    colaPrioridad.add(origen);
+
+    while (!colaPrioridad.isEmpty()) {
+      Vertice actual = colaPrioridad.poll();
+
+      for (Arista arista : grafo.getAristas(actual.valor)) {
+        Vertice vecino = arista.destino;
+        int nuevaDistancia = distancias.get(actual) + arista.peso;
+        if (nuevaDistancia < distancias.get(vecino)) {
+          distancias.put(vecino, nuevaDistancia);
+          colaPrioridad.add(vecino);
+        }
+      }
+    }
+  }
+
+  public int getDistancia(Vertice vertice) {
+    return distancias.get(vertice);
+  }
+
+  public Arista mejorCentro(Centro[] centros) {
+    int mejorDistancia = Integer.MAX_VALUE;
+    Centro mejor = null;
+    for (Centro centro : centros) {
+      int distancia = this.getDistancia(centro);
+      if (distancia < mejorDistancia) {
+        mejorDistancia = distancia;
+        mejor = centro;
+      }
+    }
+   return new Arista(origen, mejor, mejorDistancia);
+  }
+}
