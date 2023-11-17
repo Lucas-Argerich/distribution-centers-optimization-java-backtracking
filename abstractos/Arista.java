d8cd7009b95c302185557b053af5ec949bf26a29
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-11-17 19:34:54 -0300

    Removed unnecesary Arista comparable implementation

--- abstractos/Arista.java
+++ abstractos/Arista.java
@@ -1,6 +1,6 @@
 package abstractos;
 
-public class Arista implements Comparable<Arista> {
+public class Arista {
   public Vertice origen;
   public Vertice destino;
   public int peso;
@@ -10,10 +10,5 @@ public class Arista implements Comparable<Arista> {
     this.origen = origen;
     this.destino = destino;
     this.peso = peso;
-  }
-
-  @Override
-  public int compareTo(Arista otro) {
-    return Integer.compare(this.peso, otro.peso);
   }
 }
